4549357eb32b5cc16bf070c08b0ce5599bffe6af
Vikas Bahuguna <vikasedu10@gmail.com>
2023-06-07 20:06:09 +0530

    Fix env prints

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { RoutingModule } from "./containers/RoutingModule";
 
 function App() {
     console.log("################################")
-    console.log("DNS_URL: ", process.env.DNS_URL)
-    console.log("KEYCLOAK_URL: ", process.env.KEYCLOAK_URL)
-    console.log("KEYCLOAK_REALM_NAME: ", process.env.KEYCLOAK_REALM_NAME)
-    console.log("KEYCLOAK_CLIENT_ID: ", process.env.KEYCLOAK_CLIENT_ID)
+    console.log("REACT_APP_DNS_URL: ", process.env.REACT_APP_DNS_URL)
+    console.log("REACT_APP_KEYCLOAK_URL: ", process.env.REACT_APP_KEYCLOAK_URL)
+    console.log("REACT_APP_KEYCLOAK_REALM_NAME: ", process.env.REACT_APP_KEYCLOAK_REALM_NAME)
+    console.log("REACT_APP_KEYCLOAK_CLIENT_ID: ", process.env.REACT_APP_KEYCLOAK_CLIENT_ID)
     console.log("################################")
     return (
         <RoutingModule />
